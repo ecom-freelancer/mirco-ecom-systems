19e5367508e188f7db87f3d1f472ef24d18d5c0d
Quang Anh <quanganhpham31101998@gmail.com>
2024-03-21 23:12:23 +0700

    feat: API register

--- sites/consumer-server/src/modules/customer/customer.module.ts
+++ sites/consumer-server/src/modules/customer/customer.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CustomerService } from './customer.service';
+import { MysqlModule } from '@packages/nest-mysql';
 
 @Module({
+  imports: [MysqlModule.getMysqlModule()],
   providers: [CustomerService],
   exports: [CustomerService],
 })
--- sites/consumer-server/src/modules/customer/customer.service.ts
+++ sites/consumer-server/src/modules/customer/customer.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { CustomerEntity, InjectRepository } from '@packages/nest-mysql';
+import { Repository } from 'typeorm';
+import { CreateCustomerPayload } from './interfaces/create-customer.interface';
 
 @Injectable()
-export class CustomerService {}
+export class CustomerService {
+  constructor(
+    @InjectRepository(CustomerEntity)
+    private readonly customerRepository: Repository<CustomerEntity>,
+  ) {}
+
+  async checkDuplicatedCustomer({
+    email,
+    phone,
+  }: {
+    email?: string;
+    phone?: string;
+  }): Promise<boolean> {
+    const condition = [];
+    if (!!email) {
+      condition.push({ email });
+    }
+
+    if (!!phone) {
+      condition.push({ phone });
+    }
+
+    return (await this.customerRepository.count({ where: condition })) !== 0;
+  }
+
+  async createCustomer(payload: CreateCustomerPayload) {
+    const customer = this.customerRepository.create({
+      email: payload.email,
+      password: payload.password,
+      phone: payload.phone,
+      firstName: payload.firstName,
+      lastName: payload.lastName,
+    });
+
+    return await this.customerRepository.save(customer);
+  }
+}
